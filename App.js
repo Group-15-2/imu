0c733619bad0d2b5736a6c13ab84b807626883c4
achira5857 <dinonachira123@gmail.com>
2022-11-06 20:51:57 +0530

    modify

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>api dn Mk</Text>
+      <Text>mukuth ne</Text>
       <StatusBar style="auto" />
     </View>
   );
