6b4391440d96aca2ae272d5a47a05b0394f2b54d
MalinduMax <malindumaxxa0@gmail.com>
2022-11-30 17:46:13 +0530

    Icon

--- App.js
+++ App.js
@@ -10,7 +10,32 @@ const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Home') {
+          iconName = focused
+            ? 'home'
+            : 'home-outline';
+        } else if (route.name === 'Settings') {
+          iconName = focused ? 'ios-list' : 'ios-list-outline';
+        }
+        else if (route.name === 'Messages') {
+          iconName = focused ? 'mail' : 'mail-outline';
+        }
+        else if (route.name === 'Thoughts') {
+          iconName = focused ? 'star' : 'star-outline';
+        }
+
+        // You can return any component that you like here!
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: 'tomato',
+      tabBarInactiveTintColor: 'gray',
+    })}
+  >
       <Tab.Screen name="Home" component={homeScreen} />
       <Tab.Screen name="Messages" component={settingsScreen} />
       <Tab.Screen name="+" component={settingsScreen} />
