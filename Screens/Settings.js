6c20149b69f5068b1eb68ebbd858870e6fa9a644
MalinduMax <malindumaxxa0@gmail.com>
2022-11-30 22:19:51 +0530

    Settings

--- Screens/Settings.js
+++ Screens/Settings.js
@@ -1,10 +1,20 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { SectionList, Text, View } from 'react-native';
+import setstyles from '../styles/SettingsStyles';
 
-
-export default function Settings() {  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>This is the settings screen</Text>
+export default function Settings() {
+  return (
+    <View style={setstyles.container}>
+      <SectionList
+        sections={[
+          {title: 'Profile', data: ['User Name', 'Profile Details', 'Profile Picture']},
+          {title: 'Privacy', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+          {title: 'Log Out', data: ['Log Out', 'Change Account']},
+        ]}
+        renderItem={({item}) => <Text style={setstyles.item}>{item}</Text>}
+        renderSectionHeader={({section}) => <Text style={setstyles.sectionHeader}>{section.title}</Text>}
+        keyExtractor={(item, index) => `basicListEntry-${item.title}`}
+      />
     </View>
   );
-};
+}
---
+++ styles/SettingsStyles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 16,
+    fontWeight: 'bold',
+    backgroundColor: 'darkgray',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
+
+export default styles 
