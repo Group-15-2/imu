4e7fa61693d21df60779a9886a11b37156c6fc3d
MalinduMax <malindumaxxa0@gmail.com>
2022-12-01 09:07:08 +0530

    ss

--- Screens/Home.js
+++ Screens/Home.js
@@ -7,6 +7,11 @@ import Card from '../components/card';
 export default function Home() {
   return (
     <SafeAreaView>
+      <View>
+      <Text style={{ fontSize: 20, fontWeight: 'bold', marginLeft: 16, paddingTop: 30, color: '#1877F2' }}>
+          How Are You Feeling Today ?
+      </Text>
+      </View>
       <Card />
     </SafeAreaView>
 
