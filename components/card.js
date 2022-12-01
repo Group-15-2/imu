3a924ef70cbb1845002441164836746f3ba34340
M. D. Ipalawatta <d.ipalawatta@gmail.com>
2022-12-01 23:44:22 +0530

    card

--- components/card.js
+++ components/card.js
@@ -5,6 +5,15 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 
 
 export default function Card() {
+    var postTextOriginal = "jfgsjdkfhkdsjhfkseheukfhsukdjfshdfk dksjfhskeuhkdudjfkskbfjdskjfhdskjf skjdhfkseuhfkesudkhjfhdkjsheufhskdjfhskjdfhs skdjfhesuk ksjhfksdjfhseuksudoifjslejfskjdh skjdhfkseuhfkesudkhjfhdkjsheufhskdjfhskjdfhs";
+    var seeMore;
+    var postTextProcessed;
+
+    if (postTextOriginal.length > 20) {
+        postTextProcessed = "sg" + " see more....";
+    } else {
+        seeMore = "";
+    }
     return (
         <View style={cardStyles.card}>
             <View style={cardStyles.card_head}>
@@ -16,7 +25,8 @@ export default function Card() {
             </View>
 
             <View style={{ backgroundColor: '#8EDD81', width: '100%', height: 290, }}>
-                <Text style={cardStyles.post_text}>dsfsudh skjdhfksjhfkjs skdjhfksjdhfkjsdh ksjdhkfsjdh</Text>
+                <Text style={cardStyles.post_text}>{postTextProcessed}</Text>
+                <Text>{seeMore}</Text>
             </View>
 
         </View>
@@ -57,9 +67,14 @@ const cardStyles = StyleSheet.create({
         color: "#504F4F",
     },
     post_text: {
+        textAlignVertical: 'center',
         textAlign: 'center',
         fontSize: 32,
         fontWeight: 'bold',
-        color: '#FFFFFF'
+        color: '#FFFFFF',
+        height: 290,
+
+
+
     }
 })
