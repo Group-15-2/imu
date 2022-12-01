e4403bd127f99365a0a99950f939472cf25b9e6c
MalinduMax <malindumaxxa0@gmail.com>
2022-12-01 10:44:18 +0530

    ss

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { Text, View, SafeAreaView } from 'react-native';
+import { Text, View, SafeAreaView, Button } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 import Card from '../components/card';
 
 
@@ -7,11 +8,22 @@ import Card from '../components/card';
 export default function Home() {
   return (
     <SafeAreaView>
-      <View>
-      <Text style={{ fontSize: 20, fontWeight: 'bold', marginLeft: 16, paddingTop: 30, color: '#1877F2' }}>
+      <View style={{backgroundColor: "#fff",
+        margin: 10,
+        borderRadius: 10,
+        height: 50, flexDirection: 'row'}}>
+      <Text style={{ fontSize: 20, fontWeight: 'bold', marginLeft: 16, paddingTop: 10, color: '#1877F2' }}>
           How Are You Feeling Today ?
       </Text>
+      <button style={{ marginLeft: "auto", backgroundColor: '#add8e6', borderColor: '#fff', borderTopLeftRadius: 20, borderBottomLeftRadius: 20 }}>
+          <Icon
+        name='add-circle-outline'
+        size={35}
+        color='#black'
+        style={{height:25,width:25}}/>
+      </button>
       </View>
+      <Card />
       <Card />
     </SafeAreaView>
 
