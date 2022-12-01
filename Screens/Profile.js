6b2e2f7c3a87106ebbb2f301ac059901d29fe8d7
MalinduMax <malindumaxxa0@gmail.com>
2022-12-01 21:38:29 +0530

    ss

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import { SectionList, Text, View } from 'react-native';
-// import setstyles from '../styles/SettingsStyles';
+import { Text, View, Image } from 'react-native';
+//import fStyles from '../styles/feedStyle';
 
 export default function Profile() {
   return (
     <View >
-      <Text style={{ fontSize: 20, fontWeight: 'bold',  marginLeft: 16, paddingTop: 10, color: '#1877F2' }}>
+      <Text style={{ fontSize: 30, fontWeight: 'bold',  marginLeft: 16, paddingTop: 10, color: '#1877F2' }}>
           Profile
       </Text>
     </View>
