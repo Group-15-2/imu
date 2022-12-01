4a067872b96428ff5ed88ab23b113b78f5e14243
M. D. Ipalawatta <d.ipalawatta@gmail.com>
2022-12-01 21:21:08 +0530

    error fix

--- Screens/Messages.js
+++ Screens/Messages.js
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
-import Card from '../components/card';
 import { SearchBar } from 'react-native';
 
 
 export default function Messages() {
   return (
     <View >
-      <Text style={{ fontSize: 20, fontWeight: 'bold',  marginLeft: 16, paddingTop: 10, color: '#1877F2' }}>
-          Chats
+      <Text style={{ fontSize: 20, fontWeight: 'bold', marginLeft: 16, paddingTop: 10, color: '#1877F2' }}>
+        Chats
       </Text>
     </View>
   );
