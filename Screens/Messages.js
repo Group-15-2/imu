cd665a9db4f64f1fcaf1ea69a808b810e94be03a
MalinduMax <malindumaxxa0@gmail.com>
2022-12-03 09:27:03 +0530

    chat

--- Screens/Messages.js
+++ Screens/Messages.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {Text, View, Image, SafeAreaView, Switch, TouchableOpacity, TextInput} from 'react-native';
-import {styled} from '../styles/feedStyle';
 import {globalStyles} from '../styles/global';
 import { useFonts } from 'expo-font';
 
@@ -8,24 +7,28 @@ import { useFonts } from 'expo-font';
 
 export default function Messages() {
   return (
-    <View style={globalStyles.wrapper}>
-
+    <View>
       <Text style={globalStyles.header}>
         Chats
       </Text>
-
       <View>
-
-        <TouchableOpacity>
-          <View style={globalStyles.namePicContainer}>
-            <View>
-              <Image source={require('../assets/test_profile_image.jpg')} style={globalStyles.userimg}/>
-              <Image source={require('../assets/moodlets/happy.png')} style={globalStyles.moodlet}/>
+        <TouchableOpacity style={globalStyles.card}>
+            <View style={{flexDirection:'row'}}>
+              <View style={globalStyles.c1}>
+                <View style={globalStyles.namePicContainer}>
+                  <View> 
+                    <Image source={require('../assets/test_profile_image.jpg')} style={globalStyles.userimg}/>
+                    <Image source={require('../assets/moodlets/happy.png')} style={globalStyles.moodlet}/>
+                  </View>
+                  <View>
+                    <Text>Name</Text>
+                    <Text>Name</Text>
+                  </View>
+              </View>
             </View>
-            <View>
-              <Text>Name</Text>
-              <Text>Name</Text>
-            </View>
+            <View style={globalStyles.c2}>
+              <Text style={globalStyles.txt}>15.02 PM</Text>
+            </View>  
           </View>
         </TouchableOpacity>
       </View>
