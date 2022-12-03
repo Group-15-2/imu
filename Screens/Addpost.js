ebefeaf5d342c9b9fe028b6ec24f64e88bfde2ab
M. D. Ipalawatta <d.ipalawatta@gmail.com>
2022-12-03 19:15:13 +0530

    add post

--- Screens/Addpost.js
+++ Screens/Addpost.js
@@ -1,16 +1,43 @@
 import * as React from 'react';
-import { SectionList, Text, View } from 'react-native';
-import { addStyles } from '../styles/addstyle';
+import { SectionList, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+// import { addStyles } from '../styles/addstyle';
 
 export default function Addpost() {
   return (
-    <View >
+    <View>
       <Text style={addStyles.header}>
-          Add Post
+        Add Post
       </Text>
+      <TouchableOpacity>
+        <View style={{ width: 24, height: 24, borderRadius: 50, backgroundColor: '#6562FF', borderColor: '#fff' }}></View>
+      </TouchableOpacity>
+
       <View style={addStyles.card}>
 
       </View>
     </View>
   );
-}      
+}
+
+const addStyles = StyleSheet.create({
+
+  wrapper: {
+    marginLeft: 16,
+    paddingTop: 30
+  },
+
+  header: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginLeft: 16,
+    paddingTop: 30,
+    color: '#1877F2',
+  },
+
+  card: {
+    backgroundColor: "#fff",
+    margin: 16,
+    borderRadius: 10,
+    height: 450,
+  },
+})
