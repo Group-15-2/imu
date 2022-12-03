f2b440415c118de0c75de46a252908a2bef79a2d
MalinduMax <malindumaxxa0@gmail.com>
2022-12-03 18:39:39 +0530

    edit thoughts

--- Screens/Thoughts.js
+++ Screens/Thoughts.js
@@ -4,12 +4,40 @@ import { thStyles } from '../styles/thstyle';
 
 export default function Addpost() {
   return (
-    <View >
+    <View>
       <Text style={thStyles.header}>
-          Thought Space
+        Thought Space
       </Text>
       <View style={thStyles.card}>
-
+        <View style={thStyles.content}>
+          <Text style={thStyles.date}>Today</Text>
+        </View>
+        <View style={{flexDirection:'row'}}>
+          <View style={thStyles.c1}>
+              <View style={thStyles.nameContainer}>
+              <View>
+                  <Text style={thStyles.thought}>People Need Help</Text>
+                  <Text style={thStyles.t}>People need help People expect alot and end up havin none</Text>
+              </View>
+          </View>
+          </View>
+          <View style={thStyles.c2}>
+          <Text style={thStyles.txt}>15.02 PM</Text>
+          </View>  
+        </View>
+        <View style={{flexDirection:'row'}}>
+          <View style={thStyles.c1}>
+              <View style={thStyles.nameContainer}>
+              <View>
+                  <Text style={thStyles.thought}>People Need Help</Text>
+                  <Text style={thStyles.t}>People need help People expect alot and end up havin none</Text>
+              </View>
+          </View>
+          </View>
+          <View style={thStyles.c2}>
+          <Text style={thStyles.txt}>15.02 PM</Text>
+          </View>  
+        </View>
       </View>
     </View>
   );
--- styles/thstyle.js
+++ styles/thstyle.js
@@ -21,6 +21,53 @@ const thStyles = StyleSheet.create({
         borderRadius: 10,
         height: 450,
     },
+
+    content: {
+        marginLeft: 10,
+        marginTop: 10
+    },
+
+    date: {
+        fontSize: 16,
+        fontWeight: '800',
+        color: '#64A4F6'
+    },
+
+    c1: {
+        width: '85%'
+    },
+
+    c2: {
+        width: '15%'
+    },
+
+    nameContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginTop: 10
+    },
+
+    t: {
+        fontSize: 10,
+        fontWeight: '300',
+        color: "#504F4F",
+        marginTop: 5
+    },
+
+    txt: {
+        justifyContent: 'center',
+        marginTop: 25,
+        fontSize: 10,
+        fontWeight: '600',
+        color:'#504F4F'
+    },
+
+    thought: {
+        fontSize: 16,
+    }
+
 })
 
 export { thStyles }
