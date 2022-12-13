21e9cb2352b2996c7cb94c9f289ae4523984a461
MalinduMax <malindumaxxa0@gmail.com>
2022-12-13 19:19:46 +0530

    Changes

--- styles/instyle.js
+++ styles/instyle.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StyleSheet } from "react-native";
 
 const inStyle = StyleSheet.create ({
@@ -42,48 +41,3 @@ const inStyle = StyleSheet.create ({
 })
 
 export  { inStyle }
-=======
-import { StyleSheet } from "react-native";
-
-const inStyle = StyleSheet.create ({
-
-    head: {
-        fontSize: 32,
-        fontWeight: '800',
-        marginLeft: 10,
-        alignItems: 'flex-start'
-    },
-
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        backgroundColor: '#fff'
-    },
-    inputContainer: {
-        margin: 6,
-        width: '95%',
-        borderRadius: 10,
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: '#ECECEC'
-    },
-    inputField: {
-        flexDirection: 'row',
-        color: '#BBBBBB',
-        padding: 12,
-        paddingRight: 0,
-        fontSize: 24,
-        fontWeight: '900',
-        width: '90%'
-    },
-    txtInt: {
-        fontSize: 16,
-        borderRadius: 10,
-        margin: 10,
-        width: '95%',
-        borderRadius: 10,
-    }
-})
-
-export  { inStyle }
->>>>>>> 61aa77f4afb0597d3076101e064f20974a182d5f
