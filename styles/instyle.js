61aa77f4afb0597d3076101e064f20974a182d5f
Malindu Wasalamudali <117573770+MalinduMax@users.noreply.github.com>
2022-12-13 19:08:57 +0530

    Add files via upload

---
+++ styles/instyle.js
@@ -0,0 +1,43 @@
+import { StyleSheet } from "react-native";
+
+const inStyle = StyleSheet.create ({
+
+    head: {
+        fontSize: 32,
+        fontWeight: '800',
+        marginLeft: 10,
+        alignItems: 'flex-start'
+    },
+
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#fff'
+    },
+    inputContainer: {
+        margin: 6,
+        width: '95%',
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#ECECEC'
+    },
+    inputField: {
+        flexDirection: 'row',
+        color: '#BBBBBB',
+        padding: 12,
+        paddingRight: 0,
+        fontSize: 24,
+        fontWeight: '900',
+        width: '90%'
+    },
+    txtInt: {
+        fontSize: 16,
+        borderRadius: 10,
+        margin: 10,
+        width: '95%',
+        borderRadius: 10,
+    }
+})
+
+export  { inStyle }
