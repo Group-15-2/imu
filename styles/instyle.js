e26d293e568c71a16227a2d5fc9905957f2a532f
MalinduMax <malindumaxxa0@gmail.com>
2022-12-13 19:40:49 +0530

    Update Sign in Screen

--- styles/instyle.js
+++ styles/instyle.js
@@ -12,10 +12,11 @@ const inStyle = StyleSheet.create ({
     container: {
         flex: 1,
         justifyContent: 'center',
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
+        width:'100%'
     },
     inputContainer: {
-        margin: 6,
+        margin: 7,
         width: '95%',
         borderRadius: 10,
         flexDirection: 'row',
@@ -26,17 +27,23 @@ const inStyle = StyleSheet.create ({
         flexDirection: 'row',
         color: '#BBBBBB',
         padding: 12,
-        paddingRight: 0,
         fontSize: 24,
         fontWeight: '900',
         width: '90%'
     },
     txtInt: {
         fontSize: 16,
-        borderRadius: 10,
-        margin: 10,
+        margin: 7,
         width: '95%',
+        height: 48,
         borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#1877F2',
+    },
+    txt: {
+        fontSize: 16,
+        color: '#fff'
     }
 })
 
