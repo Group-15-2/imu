33bd1e700d284be3df1e9e2f560cf45d74b3b0a3
MalinduMax <malindumaxxa0@gmail.com>
2022-12-13 19:09:13 +0530

    Add Sign Screen

--- Screens/SignIn.js
+++ Screens/SignIn.js
@@ -1,12 +1,45 @@
-import * as React from 'react';
-import { Text, View, Button } from 'react-native';
+import React,{ useState } from 'react';
+import { Pressable, TextInput, View, Text, Button } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { inStyle } from '../styles/instyle';
 
-export default function SignIn ({navigation}) {
-    return (
-        <View style={{flex:1, alignItems: 'center', justifyContent: 'center' }}>
-            <Button title="Login" onPress={() => navigation.navigate('Home')}/>
-            <Text>or</Text>
-            <Button title="Sign Up" onPress={() => navigation.navigate('SignUp')}/>
+import { useTogglePasswordVisibility } from '../styles/useTogglePasswordVisibility';
+
+export default function App({ navigation }) {
+  const { passwordVisibility, rightIcon, handlePasswordVisibility } =
+    useTogglePasswordVisibility();
+  const [password, setPassword] = useState('');
+
+  return (
+    <View style={inStyle.container}>
+        <Text style={inStyle.head}>Sign In</Text>
+        <View style={inStyle.inputContainer}>
+            <View style={inStyle.inputField}>
+            <MaterialCommunityIcons  name={'email'} size={22} color={'#BBBBBB'}/>
+            <TextInput style={{fontSize:16}} placeholder="Email"/>
+            </View>
         </View>
-    );
+        <View style={inStyle.inputContainer}>
+            <View style={inStyle.inputField}>
+            <MaterialCommunityIcons  name={'lock'} size={22} color={'#BBBBBB'}/>    
+            <TextInput
+            name="password"
+            placeholder="Password"
+            style={{fontSize:16}}
+            autoCapitalize="none"
+            autoCorrect={false}
+            textContentType="newPassword"
+            secureTextEntry={passwordVisibility}
+            value={password}
+            enablesReturnKeyAutomatically
+            onChangeText={text => setPassword(text)}
+            />
+            </View>
+            <Pressable onPress={handlePasswordVisibility}>
+            <MaterialCommunityIcons name={rightIcon} size={22} color="#232323" />
+            </Pressable> 
+        </View>
+        <Button style={inStyle.txtInt} title="Login" onPress={() => navigation.navigate('Home')}/>
+    </View>
+  );
 }
