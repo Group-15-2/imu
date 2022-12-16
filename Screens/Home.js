0c8d82e7a3fe6e3e5953952304e428fd2614c648
MalinduMax <malindumaxxa0@gmail.com>
2022-12-16 16:40:53 +0530

    Changes in moodlets

--- Screens/Home.js
+++ Screens/Home.js
@@ -29,7 +29,7 @@ const moodletDATA = [
   },
   {
     id: 4,
-    link: require('../assets/moodlets/anguish.png'),
+    link: require('../assets/moodlets/Anguish.png'),
     mood: 'Feeling Anguish!'
   },
   {
