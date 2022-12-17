f4aab3c1248a1c28fafde1746dff25e601b1ab71
MalinduMax <malindumaxxa0@gmail.com>
2022-12-17 22:04:52 +0530

    Changes

--- Screens/ChatBox.js
+++ Screens/ChatBox.js
@@ -1,1 +1,12 @@
+import React,{ useState, useRef } from 'react';
+import { Pressable, TextInput, View, Text, TouchableOpacity } from 'react-native';
+import { inStyle } from '../styles/instyle';
 
+export default function ChatBox({ navigation }) {
+
+  return (
+    <View style={inStyle.container}>
+        <Text style={inStyle.head}>Chat Box</Text>
+    </View>
+  );
+}  
