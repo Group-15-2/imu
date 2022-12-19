158aaf4a9922f58aaa15c4cb85c39559c968ef5a
M. D. Ipalawatta <d.ipalawatta@gmail.com>
2022-12-19 19:20:22 +0530

    sign in - fixed!

--- Screens/SignIn.js
+++ Screens/SignIn.js
@@ -3,7 +3,8 @@ import { Pressable, TextInput, View, Text, TouchableOpacity, Image } from 'react
 import { Divider, SocialIcon } from 'react-native-elements';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { inStyle } from '../styles/instyle';
-
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../firebaseConfig';
 import { useTogglePasswordVisibility } from '../styles/useTogglePasswordVisibility';
 
 export default function App({ navigation }) {
@@ -13,6 +14,26 @@ export default function App({ navigation }) {
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
 
+    //register users when trigger
+    const handleSignIn = () => {
+        signInWithEmailAndPassword(auth, email, password)
+            .then(() => {
+                console.log('User signed in!');
+                navigation.navigate('Home');
+
+            })
+            .catch(error => {
+                if (error.code === 'auth/email-already-in-use') {
+                    console.log('That email address is already in use!');
+                }
+
+                if (error.code === 'auth/invalid-email') {
+                    console.log('That email address is invalid!');
+                }
+
+                console.error(error);
+            });
+    };
 
     return (
         <View style={inStyle.container}>
@@ -48,7 +69,7 @@ export default function App({ navigation }) {
                     <MaterialCommunityIcons name={rightIcon} size={22} color="#BBBBBB" />
                 </Pressable>
             </View>
-            <TouchableOpacity activeOpacity={.7} style={inStyle.txtInt} onPress={() => navigation.navigate('Home')}>
+            <TouchableOpacity activeOpacity={.7} style={inStyle.txtInt} onPress={handleSignIn}>
                 <Text style={inStyle.txt}>Login</Text>
             </TouchableOpacity>
             <Divider style={inStyle.divider} />
